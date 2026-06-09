685289e92534dfc4feb3e7cdf1580db8e0ac8f50
Giovani-Rabbit <giovanicoelho2004@hotmail.com>
2026-06-08 22:20:58 -0300

    remove a test

--- tests/integration.rs
+++ tests/integration.rs
@@ -11,18 +11,3 @@ fn test_csv_filter() {
 
     assert!(output.status.success());
 }
-
-#[test]
-fn test_csv_filter_invalid_operator() {
-    let output = Command::new("cargo")
-        .args(["run", CSV_FILE, "filter", "Identifier1000"])
-        .output()
-        .unwrap();
-
-    let stderr = String::from_utf8(output.stderr).unwrap();
-
-    println!("stderr: {stderr}");
-
-    assert!(!output.status.success());
-    assert!(stderr.contains("No valid operator could be found"))
-}

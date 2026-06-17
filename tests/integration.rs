4b82875806a3894cf8f21e2e554e7f06feb3df0b
Giovani-Rabbit <giovanicoelho2004@hotmail.com>
2026-06-16 23:09:30 -0300

    integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -11,3 +11,35 @@ fn test_csv_filter() {
 
     assert!(output.status.success());
 }
+
+#[test]
+fn test_csv_show() {
+    let output = Command::new("cargo")
+        .args(["run", CSV_FILE, "show"])
+        .output()
+        .expect("falhou ao executar");
+
+    assert!(output.status.success());
+
+    let stdout = String::from_utf8(output.stdout).expect("stdout não é UTF-8 válido");
+
+    assert!(stdout.contains("Username  | Identifier | First name | Last name"));
+    assert!(stdout.contains("----------+------------+------------+----------"));
+    assert!(stdout.contains("johnson81 | 4081       | Craig      | Johnson"));
+}
+
+#[test]
+fn test_csv_json() {
+    let output = Command::new("cargo")
+        .args(["run", CSV_FILE, "export"])
+        .output()
+        .expect("falhou ao executar");
+
+    assert!(output.status.success());
+
+    let stdout = String::from_utf8(output.stdout).expect("stdout não é UTF-8 válido");
+
+    assert!(stdout.contains("\"Craig\""));
+    assert!(stdout.contains("\"Username\": \"grey07\""));
+    assert!(stdout.contains("\"Last name\": \"Smith\""));
+}
